6bb118745230591e0e38691aa09bf16fa6a343fb
user <user@DESKTOP-COJ9858>
2022-11-15 17:48:17 +0900

    221115 chapt7정리

--- Chapter7_정리/src/com/sist/main/MainClass_정리.java
+++ Chapter7_정리/src/com/sist/main/MainClass_정리.java
@@ -1,10 +1,24 @@
 package com.sist.main;
 
+// 디자인 패턴 => 싱글톤 패턴 (메모리 누수 방지)
+class A{
+	private static A a;
+	public static A newInstance() {
+		if(a==null)
+			a=new A();
+		return a;
+	}
+}
 public class MainClass_정리 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		A a=A.newInstance();
+		System.out.println("a="+a);
+		A b=A.newInstance();
+		System.out.println("b="+b);
+		A c=A.newInstance();
+		System.out.println("c="+c);
 	}
 
 }
