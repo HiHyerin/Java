8dc569a239b77f44717f30d4a12058132663f58e
HiHyerin <116174672+HiHyerin@users.noreply.github.com>
2022-11-29 16:33:12 +0900

    221129 java총정리

---
+++ 자바총정리_221129/src/com/sist/main/MainClass.java
@@ -0,0 +1,13 @@
+package com.sist.main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+// 노션 https://www.notion.so/a8537f8370154b44b5a060bcbd43aa4a
